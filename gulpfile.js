ab31638c7911d25b0ee256c5107628dd70996b58
ebrewe <ericdbrewer@gmail.com>
2016-04-15 15:40:21 -0400

    gulp workflow added

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('./webpack.config');
+
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var watch = require('gulp-watch');
+
+//paths
+var DIST_PATH = "public/dist/";
+var STYLE_PATH = "style/**/*.scss";
+
+gulp.task("default", ['watch', 'webpack-dev-server']);
+
+gulp.task("watch", function(){
+  return gulp.watch([STYLE_PATH], ['styles']);
+});
+
+gulp.task("styles", function(){
+    console.log('running styles')
+    return gulp.src(STYLE_PATH)
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        output:'compressed'
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(DIST_PATH + 'style/'));
+});
+
+gulp.task("webpack-dev-server", function(callback) {
+	// modify some webpack config options
+	var myConfig = Object.create(webpackConfig);
+	myConfig.devtool = "eval";
+	myConfig.debug = true;
+
+	// Start a webpack-dev-server
+	new WebpackDevServer(webpack(myConfig), {
+		publicPath: "/" + myConfig.output.publicPath,
+		stats: {
+			colors: true
+		}
+	}).listen(8080, "localhost", function(err) {
+		if(err) throw new gutil.PluginError("webpack-dev-server", err);
+		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+	});
+});
