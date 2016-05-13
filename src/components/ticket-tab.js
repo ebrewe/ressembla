cef90f45b814d1919d26d6b75d9fe7e631b35a7e
eric.brewer <eric.brewer@sandboxww.com>
2016-05-13 09:03:40 -0400

    replaced jquery DOM with react

--- src/components/ticket-tab.js
+++ src/components/ticket-tab.js
@@ -2,77 +2,91 @@ import React, { Component } from 'react';
 //import { connect } from 'react-redux';
 //import {fetchTickets} from '../actions/index';
 import $ from 'jquery';
+import _ from 'lodash';
 
 export default class TicketTab extends Component {
   constructor(props){
     super(props);
+    this.state = {tickets: []}; 
   }
   componentWillMount(){
     var self= this;
     this.url = "";
-
       $.get(this.props.ticketUrl)
       .success((data)=>{
         this.parseData(data);
-        this.reStyle();
       })
-      /*this.props.fetchTickets(); 
-      console.log(this.props)*/
+  }
+  setLocalStorage(key, item){
+    var self = this;
+    let obj ={}
+    obj[key] = item
+    let storage = chrome.storage.local;
+    storage.get(key, (result)=>{
+      storage.set(obj);
+    });
+
   }
   parseData(data){
+    var self = this;
      var el = document.createElement('html'),
          tEl = document.getElementById(this.props.containerID);
          el.innerHTML = data;
       let links = [];
-         $(el).find('tr[id^="ticket_"]').each(function(){
-            let link= $(this).attr('data-linkUrl'),
-                number = $(this).find('td.number').find('p').text(),
-                summary = $(this).find('.summary').find('.list-column-wrapper').text(),
-                milestone = $(this).find('.milestone_id').find('.list-column-wrapper').text(),
-                space = $(this).closest('table.listing.tickets').prev('div.s-table-header').attr('data-group-id');
-                links.push({link, number, summary, milestone, space}); 
-         })
-      var markup="";
+       $(el).find('tr[id^="ticket_"]').each(function(){
+          let link= $(this).attr('data-linkUrl'),
+              number = getItemNumber($(this).find('td.number').find('p').text()),
+              summary = $(this).find('.summary').find('.list-column-wrapper').text(),
+              milestone = $(this).find('.milestone_id').find('.list-column-wrapper').text(),
+              space = $(this).closest('table.listing.tickets').prev('div.s-table-header').attr('data-group-id');
+              links.push({link, number, summary, milestone, space}); 
+       });
       function getItemNumber(numStr){
         return numStr.match( /\d+/g );
       }
-      links.map((item)=>{
-        var numberClass = item.milestone ? 'col-sm-2' : 'col-sm-4',
-            summaryClass = item.milestone ? 'col-sm-5' : 'col-sm-10',
-            milestoneClass = item.milestone ? 'col-sm-5' : ''; 
-
-        markup += "<li class='ticket-item row' data-link='" + item.link +"' id='" + item.number + "'>" +
-            '<div class="row" style="padding:0 15px;"><span class="'+numberClass+' item-number"><i class="fa fa-star"></i>' + getItemNumber(item.number) + '</span>' +
-            '<span class="'+summaryClass+' item-summary">'+$.trim(item.summary) + '</span></div>' +
-            '<span class="'+milestoneClass+' item-milestone" title="'+item.summary+'">'+$.trim(item.milestone)+'</span>' +
-            '<div class="row space-row" style="padding:0 15px;"><span class="item-space"><i class="fa fa-globe"></i>' + item.space + '</span></div>' +
-            '</li>';
-        
-      });
-      $('#'+this.props.containerID).find('ul').html(markup);
-      $('li.ticket-item').on('click', function(e){
-
-         var url = `https://www.assembla.com/${$(this).attr('data-link')}`;
-         if(self.url != url){
-          chrome.tabs.create({url});
-          self.url = url;
-         }
-         return false;
-      }); 
+      this.setLocalStorage(`ressembla-${this.props.containerID}`, links);
+      this.setState({tickets: links})
   }
-  reStyle(){
-    $('#' + this.props.containerID +' .navigation-wrapper').css('display', 'none');
+  handleClick(url){
+    chrome.tabs.create({url});
+  }
+  renderTickets(){
+    return (
+      this.state.tickets.map( (ticket)=>{
+        return(
+          <li className="ticket-item row" data-link={ticket.link} key={`ticket-${ticket.number}`} onClick={(e)=>this.handleClick(`https://www.assembla.com/${ticket.link}`)}>
+            <div className="row" style={{"padding":"0 15px"}}>
+              <span className="item-number"><i className="fa fa-star" />{ticket.number}</span>
+              <span className="item-summary">{$.trim(ticket.summary)}</span>
+            </div>
+            <span className="item-milestone">{$.trim(ticket.milestone)}</span>
+            <div className="row space-row" style={{'padding': '0 15px'}}>
+              <span className="item-space"><i className="fa fa-globe" />{ticket.space}</span>
+            </div>
+          </li>
+        )
+      })
+    )
   }
   render(){
     const visClass = this.props.visible ? 'visible' : '';
+    if(!this.state.tickets.length){
+        return(
+        <div className={`${visClass} ticket-list-inlay container-fluid`} id={this.props.containerID}>
+          <ul className="list-ticket-list">
+              <div className="load-container">
+                <div className="circle"></div>
+              </div>
+          </ul>
+        </div>
+        );
+    }
     return(
-      <div className={`${visClass} ticket-list-inlay container-fluid`} id={this.props.containerID}>
-        <ul className="list-ticket-list">
-            <div className="load-container">
-              <div className="circle"></div>
-            </div>
-        </ul>
-      </div>
+        <div className={`${visClass} ticket-list-inlay container-fluid`} id={this.props.containerID}>
+          <ul className="list-ticket-list">
+            {this.renderTickets()}
+          </ul>
+        </div>
     );
   }
 }
--- src/reducers/tickets_reducer.js
+++ src/reducers/tickets_reducer.js
@@ -3,7 +3,6 @@ import { ACTION_TYPES } from '../actions/index';
 const INTIAL_STATE = {tickets: null}
 
 export default function(state = INTIAL_STATE, action){
-  console.log(action, action.payload, state)
   switch(action.type){
 
     case ACTION_TYPES.FETCH_TICKETS:
