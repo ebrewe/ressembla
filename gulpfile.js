7fa4408ff7e0ca50af320821eb154adb56223321
eric.brewer <eric.brewer@sandboxww.com>
2016-05-16 12:39:31 -0400

    switched star colors, added minify to migration

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,8 @@ gulp.task("migrate", function(){
     gulp.src('jquery.min.js')
       .pipe(gulp.dest('dist/'));
     gulp.src('bundle.js')
+      .pipe(sourcemaps.init())
+      .pipe(minify())
       .pipe(gulp.dest('dist/'));
     gulp.src('bundle.js')
       .pipe(gulp.dest('dist/'));
